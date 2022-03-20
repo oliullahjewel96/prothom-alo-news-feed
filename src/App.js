47a17163877d6033f974908d12e3af4605b3e0f8
Oli Ullah Jewel <oli.jewel@northsouth.edu>
2022-03-20 13:09:58 +0600

    fetched data from local json file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import Header from "./components/Header";
-
+import NewsWithImage from "./components/NewsWithImage";
+import { useState, useEffect } from "react";
 function App() {
+  const [newsData, setNewsData] = useState(null);
+
+  console.log(newsData);
+  useEffect(() => {
+    fetch("./data.json")
+      .then((res) => res.json())
+      .then((data) => setNewsData(data));
+  }, []);
+
+  const news =
+    newsData &&
+    newsData.map((newData) => (
+      <NewsWithImage key={newData.id} newData={newData} />
+    ));
   return (
     <div className="App">
       <Header />
+      {news}
     </div>
   );
 }
